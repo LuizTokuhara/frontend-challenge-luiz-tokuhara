8905e7e70da113cda0dff7cd5129e5bcbef306e3
Luiz Tokuhara <tokuharaluiz@gmail.com>
2022-04-15 11:31:06 -0300

    FT add holiday card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
+      import('./core/home/home.module').then((m) => m.HomePageModule),
   },
 ];
 
--- src/app/core/home/home.module.ts
+++ src/app/core/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HolidayCardComponent } from 'src/app/components/holiday-card/holiday-card.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, HolidayCardComponent]
 })
 export class HomePageModule {}
